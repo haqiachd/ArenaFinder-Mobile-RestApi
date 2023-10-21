5dec88bc1a5fd18e0b489447a655d02d8d1accf2
Achmad Baihaqi <haqiachd@gmail.com>
2023-10-22 01:00:16 +0700

    add validator class

---
+++ controllers/mobile/users/test_validator.php
@@ -0,0 +1,30 @@
+<?php
+require "Validator.php";
+
+header("Content-Type: application/json");
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // post request
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $nama = $_POST['name'];
+        $password = $_POST['password'];
+
+        $val = new Validator();
+
+        echo "username";
+        $val->isValidUsername($username);
+        echo "\n\n";
+        echo "email";
+        $val->isValidEmail($email);
+        echo "\n\n";
+        echo "nama";
+        $val->isValidName($nama);
+        echo "\n\n";
+        echo "password";
+        $val->isValidPassword($password);
+    }else{
+        echo "error";
+    }
+
+?>
