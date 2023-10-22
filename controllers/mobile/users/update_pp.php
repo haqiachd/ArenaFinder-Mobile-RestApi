4b8acf9e8fa2748290275668c2c189030cddd06c
Achmad Baihaqi <haqiachd@gmail.com>
2023-10-22 17:23:45 +0700

    Update update_pp.php

--- controllers/mobile/users/update_pp.php
+++ controllers/mobile/users/update_pp.php
@@ -1,18 +1,22 @@
 <?php
+
 /**
  * Digunakan untuk mengupdate photo profile user.
  */
 
- require "../../koneksi.php";
+require "../../koneksi.php";
 
 header("Content-Type: application/json");
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-        // post request
-        $email = $_POST['email'];
-        $photo = $_POST['photo'];
+    // post request
+    $email = $_POST['email'];
+    $photo = $_POST['photo'];
 
+    // cek email exist atau tidak
+    $sql = "SELECT email FROM users WHERE email = '$email' LIMIT 1";
+    if ($conn->query($sql)->num_rows == 1) {
         // saving photo
         $photo = str_replace('data:image/png;base64,', '', $photo);
         $photo = str_replace(' ', '+', $photo);
@@ -26,27 +30,28 @@ header("Content-Type: application/json");
         $result = $conn->query($sql);
 
         // jika foto profile berhasil diupdate
-        if($result === true){
+        if ($result === true) {
             $sql = "SELECT user_photo FROM users WHERE email = '$email' LIMIT 1";
             $result = $conn->query($sql);
             $photo = $result->fetch_assoc();
 
-            if($result->num_rows == 1){
-                $response = array("status"=>"success", "message"=>"photo profile berhasil diupdate", "data"=>$photo);
-            }else{
-                $response = array("status"=>"success", "message"=>"photo profile berhasil diupdate");
+            if ($result->num_rows == 1) {
+                $response = array("status" => "success", "message" => "photo profile berhasil diupdate", "data" => $photo);
+            } else {
+                $response = array("status" => "success", "message" => "photo profile berhasil diupdate");
             }
-
-        }else{
-            $response = array("status"=>"error", "message"=>"photo profile gagal diupdate");
+        } else {
+            $response = array("status" => "error", "message" => "photo profile gagal diupdate");
         }
-
-        // close koneksi
-        $conn->close();
     }else{
-        $response = array("status"=>"error", "message"=>"method is not post");
+        $response = array("status"=> "error", "message"=> "Email tidak ditemukan");
     }
 
-    // show response
-    echo json_encode($response);
-?>
+    // close koneksi
+    $conn->close();
+} else {
+    $response = array("status" => "error", "message" => "method is not post");
+}
+
+// show response
+echo json_encode($response);
